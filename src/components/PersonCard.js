87814445a47d853e2ec5ab71e7c0b1888a59db2f
garavito16 <lex_94@outlook.com>
2022-03-16 17:07:41 -0500

    nuevos cambios para core

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import './PersonCard.css';
 
 class PersonCard extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            "age" : props.person.age
+        }
+    }
+
+    aumentar = () => {
+        this.setState( { "age" : this.state.age + 1 } )
+    }
+
     render() {
         const {firstName, lastName, age, hairColor} = this.props.person;
 
         return (<div className="cuadrito">
             <h2>{firstName}, {lastName}</h2>
-            <p>Age: {age}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button className="botoncito" onClick={ this.aumentar }>Birthday Button for {firstName} {lastName}</button>
         </div>);
         
     }
